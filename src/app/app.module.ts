ec0b817b34a8c332c3b2eaeb5a208ed618f114ef
Trinanjan Nandi <trino.nandi@gmail.com>
2024-05-20 02:34:49 -0700

    Add Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UploadPdfComponent } from './upload-pdf/upload-pdf.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UploadPdfComponent
+    UploadPdfComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
