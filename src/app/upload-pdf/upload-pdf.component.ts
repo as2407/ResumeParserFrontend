d5bb5205f7ae6db1f645ee7e401313a55ab2bca1
Trinanjan Nandi <trino.nandi@gmail.com>
2024-06-04 13:10:23 -0700

    update Angular to v18

--- src/app/upload-pdf/upload-pdf.component.ts
+++ src/app/upload-pdf/upload-pdf.component.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { Component,ChangeEvent } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-upload-pdf',
